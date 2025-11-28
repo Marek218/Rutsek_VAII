abf7efcad4227ae7aa8e85343e1f37aaeb544404
MarekRutsek <marek.rutsek@gmail.com>
2025-11-29 00:35:56 +0100

    Update index view and styles for Luxor hair salon theme

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -2,38 +2,21 @@
 
 /** @var \Framework\Support\LinkGenerator $link */
 ?>
-
+<link rel="stylesheet" href="<?= $link->asset('css/root.css') ?>">
 <div class="container-fluid">
     <div class="row">
         <div class="col mt-5">
             <div class="text-center">
-                <h2>Vaííčko MVC FW</h2>
-                <h3>Version <?= App\Configuration::FW_VERSION ?></h3>
-                <img src="<?= $link->asset('images/vaiicko_logo.png') ?>" alt="Framework Logo">
-                <p>
-                    Congratulations, you have successfully installed and run the framework
-                    <strong>Vaííčko</strong> <?= App\Configuration::FW_VERSION ?>!<br>
-                    We hope that you will create a great application using this framework.<br>
-                </p>
-                <p>
-                    This simple framework was created for teaching purposes and to better understand how the MVC
-                    architecture works.<br>
-                    It is intended for students of the subject <em>web application development</em>, but not only
-                    for them.
-                </p>
+                <h1>Vitajte v kaderníctve Luxor</h1>
+
             </div>
         </div>
     </div>
     <div class="row mt-3">
         <div class="col text-center">
-            <h4>Authors</h4>
-            <div>
-                <a href="mailto:Patrik.Hrkut@fri.uniza.sk">doc. Ing. Patrik Hrkút, PhD.</a><br>
-                <a href="mailto:Michal.Duracik@fri.uniza.sk">Ing. Michal Ďuračík, PhD.</a><br>
-                <a href="mailto:Matej.Mesko@fri.uniza.sk">Ing. Matej Meško, PhD.</a><br><br>
-                &copy; 2020-<?= date('Y') ?> University of Žilina, Faculty of Management Science and Informatics,
-                Department of Software Technologies
-            </div>
+        <button class="btn btn-primary btn-lg" onclick="location.href='<?= $link->url('home.contact') ?>'">
+            Objednaj sa teraz
+        </button>
         </div>
     </div>
 </div>
